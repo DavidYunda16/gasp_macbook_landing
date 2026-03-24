3dac277a1300e673f374cd5653b9c1c49a85ac1d
DavidYunda16 <2000yunda@gmail.com>
2026-03-23 19:38:59 -0500

    Ensure ProductViewer uses ../Store

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Hero from "./Components/Hero.jsx";
 import ProductViewer from "./Components/ProductViewer.jsx";
 import gsap from 'gsap';
 import { ScrollTrigger} from "gsap/all";
-import Perfomance from "./Components/perfomance.jsx";
 import Features from "./Components/Features.jsx";
 import Highlights from "./Components/Highlights.jsx";
 import Footer from "./Components/Footer.jsx";
